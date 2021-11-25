4dec98c58a6c8f6d0a7e8b4a416fd7f522e9dc9e
Pavanaprana Dasa <pavamana1123@gmail.com>
2021-11-25 15:48:10 +0530

    basic UI

---
+++ app/myfolk/src/components/header/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Header() {
+  return (
+    <div className="app_header">
+        <img src="logo.png" className="header_logo"/>
+    </div>
+  );
+}
+
+export default Header;
