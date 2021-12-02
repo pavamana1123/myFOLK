31bb04fd2cabda786b2977b2cf45efab54927460
Pavanaprana Dasa <pavamana1123@gmail.com>
2021-12-02 15:20:41 +0530

    schema

---
+++ design/db/structure.sql
@@ -0,0 +1,174 @@
+CREATE DATABASE  IF NOT EXISTS `myfolk` /*!40100 DEFAULT CHARACTER SET utf32 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `myfolk`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: myfolk
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `calendar`
+--
+
+DROP TABLE IF EXISTS `calendar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `calendar` (
+  `date` date NOT NULL,
+  `program` varchar(45) DEFAULT NULL,
+  `eventId` varchar(45) NOT NULL,
+  `eventName` varchar(45) DEFAULT NULL,
+  `startTime` time DEFAULT NULL,
+  `endTime` time DEFAULT NULL,
+  `online` tinyint DEFAULT NULL,
+  `speaker` varchar(45) DEFAULT NULL,
+  `venue` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`eventId`),
+  KEY `program_idx` (`program`),
+  CONSTRAINT `program` FOREIGN KEY (`program`) REFERENCES `programs` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contacts` (
+  `phone` varchar(15) NOT NULL,
+  `isWAPhone` tinyint DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `vocativeName` varchar(45) DEFAULT NULL,
+  `tag` varchar(45) DEFAULT NULL,
+  `secondaryPhone` varchar(15) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `address` mediumtext,
+  `isStudent` tinyint DEFAULT NULL,
+  `institution` varchar(45) DEFAULT NULL,
+  `course` varchar(45) DEFAULT NULL,
+  `semester` int DEFAULT NULL,
+  `company` varchar(45) DEFAULT NULL,
+  `designation` varchar(45) DEFAULT NULL,
+  `highestQualification` varchar(45) DEFAULT NULL,
+  `workExperience` varchar(45) DEFAULT NULL,
+  `homeTown` varchar(45) DEFAULT NULL,
+  `skills` varchar(45) DEFAULT NULL,
+  `japaRounds` int DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`phone`),
+  UNIQUE KEY `phone_UNIQUE` (`phone`),
+  UNIQUE KEY `secondaryPhone_UNIQUE` (`secondaryPhone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `participation`
+--
+
+DROP TABLE IF EXISTS `participation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `participation` (
+  `phone` varchar(20) DEFAULT NULL,
+  `eventId` varchar(45) DEFAULT NULL,
+  `caller` varchar(45) DEFAULT NULL,
+  `response` varchar(45) DEFAULT NULL,
+  `remarks` varchar(45) DEFAULT NULL,
+  `attendance` tinyint DEFAULT NULL,
+  KEY `caller_idx` (`caller`),
+  CONSTRAINT `caller` FOREIGN KEY (`caller`) REFERENCES `roles` (`phone`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `programs`
+--
+
+DROP TABLE IF EXISTS `programs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `programs` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `startTime` time DEFAULT NULL,
+  `endTime` time DEFAULT NULL,
+  `cost` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type_idx` (`type`),
+  CONSTRAINT `type` FOREIGN KEY (`type`) REFERENCES `programtypes` (`type`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `programtypes`
+--
+
+DROP TABLE IF EXISTS `programtypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `programtypes` (
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `registrations`
+--
+
+DROP TABLE IF EXISTS `registrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registrations` (
+  `date` date NOT NULL,
+  `program` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  KEY `program_idx` (`program`),
+  KEY `phone_idx` (`phone`),
+  CONSTRAINT `phone` FOREIGN KEY (`phone`) REFERENCES `contacts` (`phone`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `programid` FOREIGN KEY (`program`) REFERENCES `programs` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `role` enum('admin','caller') NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-02 15:11:28
