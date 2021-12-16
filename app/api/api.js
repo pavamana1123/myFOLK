b3fa6cc4a0421314740868e43fa3d431ada8cb02
Pavanaprana Dasa <pavamana1123@gmail.com>
2021-12-16 15:44:29 +0530

    login

--- app/api/api.js
+++ app/api/api.js
@@ -18,11 +18,29 @@ class API {
                 const {phone, passwordHash} = body
                 this.db.query(`SELECT * from contacts where phone="${phone}" and password="${passwordHash}"`, function (error, result) {
                     if (error) throw error;
-                    res.send(result)
+                    if(result.length){
+                        if(result.length==1){
+                            if(result[0].password===passwordHash){
+                                res.status(200)
+                                res.send()
+                            }else{
+                                res.status(400)
+                                res.send({"error":"incorrect password"})
+                            }
+                        }else{
+                            res.status(500)
+                            res.send({"error":`multiple users with same phone ${phone} were found`})
+                        }
+                    }else{
+                        res.status(400);
+                        res.send({"error":"unauthorised user"})
+                    }
                   });
             default:
         }
     }
 }
+
+
 
 module.exports = API
