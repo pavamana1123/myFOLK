1cd38dc92ac1756e641e3457ca01f3ac8bd9c28e
Pavanaprana Dasa <pavamana1123@gmail.com>
2021-12-30 12:21:04 +0530

    centered

--- app/myfolk/src/pages/login/index.js
+++ app/myfolk/src/pages/login/index.js
@@ -3,14 +3,12 @@ import './index.css';
 function Login() {
   return (
     <div id="login-root">
-        <div id="login-container">
-            <img id='login-logo' src="logo.png"/>
-            <form id='login-form'>
-                <input  type="tel" placeholder='Phone' pattern="[0-9]{10}"></input>
-                <input type='password' placeholder='Password'></input>
-                <button>sdfs</button>
-            </form>
-        </div>
+          <img id='login-logo' src="logo.png"/>
+          <form id='login-form'>
+              <input  type="tel" placeholder='Phone' pattern="[0-9]{10}"></input>
+              <input type='password' placeholder='Password'></input>
+              <button>sdfs</button>
+          </form>
     </div>
   );
 }
