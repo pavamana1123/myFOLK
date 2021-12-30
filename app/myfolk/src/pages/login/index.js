b3d7d6097733f48c57f4ac8414c78a4439119b0f
Pavanaprana Dasa <pavamana1123@gmail.com>
2021-12-30 16:56:24 +0530

    ht

--- app/myfolk/src/pages/login/index.js
+++ app/myfolk/src/pages/login/index.js
@@ -1,16 +1,24 @@
 import './index.css';
+import {useState} from 'react'
 
 function Login() {
+
+  var [submitState, setSubmitState] = useState(false)
+
   return (
     <div id="login-root">
           <img id='login-logo' src="logo.png"/>
           <form id='login-form'>
-              <input  type="tel" placeholder='Phone' pattern="[0-9]{10}"></input>
-              <input type='password' placeholder='Password'></input>
-              <button>sdfs</button>
+              <input ref='login-phone' type="tel" placeholder='Phone' pattern="[0-9]{10}"></input>
+              <input ref='login-pass' type='password' placeholder='Password'></input>
+              <button disabled={submitState} onClick={onLogin}>Login</button>
           </form>
     </div>
   );
+}
+
+function onLogin(e) {
+  
 }
 
 export default Login;
