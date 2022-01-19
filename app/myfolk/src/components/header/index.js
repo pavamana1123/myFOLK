3aeeb2080414341740b787793b48a5da7e829c85
Pavanaprana Dasa <pavamana1123@gmail.com>
2022-01-19 14:16:47 +0530

    added menu buttons

--- app/myfolk/src/components/header/index.js
+++ app/myfolk/src/components/header/index.js
@@ -3,7 +3,9 @@ import './index.css';
 function Header() {
   return (
     <div className="app_header">
+        <i class="bi bi-list" id="slideMenuButton"></i>
         <img src="logo.png" className="header_logo"/>
+        <i class="bi bi-three-dots-vertical" id="menuButton"></i>
     </div>
   );
 }
