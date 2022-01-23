56f39f3698dcc969b42eb07084601fe176afda7b
Pavanaprana Dasa <pavamana1123@gmail.com>
2022-01-23 14:38:53 +0530

    made icon squres

--- app/myfolk/src/components/header/index.js
+++ app/myfolk/src/components/header/index.js
@@ -4,9 +4,9 @@ function Header(props) {
   var {onPageMenuClick} = props
   return (
     <div className="app_header">
-        <i className="bi bi-list clickable" id="slideMenuButton"></i>
+        <i className="bi bi-list clickable square" id="slideMenuButton"></i>
         <img src="logo.png" className="header_logo"/>
-        <i className="bi bi-three-dots-vertical clickable" id="menuButton" onClick={onPageMenuClick}></i>
+        <i className="bi bi-three-dots-vertical clickable square" id="menuButton" onClick={onPageMenuClick}></i>
     </div>
   );
 }
