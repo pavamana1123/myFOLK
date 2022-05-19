e6dc39fbc9863f9e8b81085d4a4ca61b5a88029e
Pavanaprana Dasa <pavamana1123@gmail.com>
2022-05-19 16:01:14 +0530

    ok

--- app/myfolk/src/index.js
+++ app/myfolk/src/index.js
@@ -11,6 +11,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+if ("serviceWorker" in navigator) {
+  // register service worker
+  navigator.serviceWorker.register("service-worker.js");
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
